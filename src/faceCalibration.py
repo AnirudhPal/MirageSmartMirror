b5a6663b218778217856fc53a238c0a159513ba6
Sultan Alshamsi <salshams@purdue.edu>
2018-10-30 12:47:15 -0400

    faceCalibration added

--- src/faceCalibration.py
+++ src/faceCalibration.py
@@ -1,12 +1,21 @@
 from time import sleep
 from picamera import PiCamera
+import os
 
 def faceCalibration:
     camera = PiCamera()
-    camera.start_preview()
+    # camera.start_preview()
+    path = "./Users/%/" % name
+    try:
+        os.mkdir(path)
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
+
     for i in range(5):
         sleep(5)
-        camera.capture('./Users/%/image%s.jpg' % (name , i))
-    camera.stop_preview()
-    
+        camera.capture('./Users/%s/image%s.jpg' % (name , i))
+    # camera.stop_preview()
+
 faceCalibration("Andrew0")
