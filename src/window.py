a680abc10a6f25db56e88de806728cb1cd2695d1
azahraa <31976482+azahraa@users.noreply.github.com>
2018-12-07 01:08:51 -0500

    debugging

--- src/window.py
+++ src/window.py
@@ -689,7 +689,7 @@ class Window(QWidget):
             #     data = json.load(f)
             # dict = json.loads(data)
             self.google_code = dict['user_code']
-            print(self.google_code)
+            # print(self.google_code)
             self.show_auth_code()
             self.googleCodeTimeout = 10
             return False
